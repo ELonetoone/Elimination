c4d1682d3fc449b58c1ab4b2d00d9acdd73bb0f0
訾源 <161250220@smail.nju.edu.cn>
2017-04-30 20:09:31 +0800

    新增市场

---
+++ src/el/onetoone/back/ItemList.java
@@ -0,0 +1,25 @@
+package el.onetoone.back;
+
+/**
+ * 存储所有道具的名称常量
+ * @author iznauy
+ * 
+ */
+public class ItemList {
+	
+	/**
+	 * 价格..5000?
+	 */
+	public static final String HAMMER = "一个可以砸掉某个宝石的小锤子";
+	
+	/**
+	 * 价格..10000?
+	 */
+	public static final String BOOM = "可以炸掉某一行";
+	
+	/**
+	 * 价格...10000?
+	 */
+	public static final String NEWMAP = "可以生成一张全新地图";
+	
+}
---
+++ src/el/onetoone/back/Market.java
@@ -0,0 +1,55 @@
+package el.onetoone.back;
+
+import java.util.HashMap;
+
+/**
+ * 市场类
+ * @author iznauy
+ * 工具类
+ */
+public class Market {
+	
+	/**
+	 * 存放商品列表（商品名-价格）
+	 */
+	private static HashMap<String, Integer> itemList = new HashMap<>();
+	
+	/**
+	 * 初始化商品列表
+	 */
+	static {
+		itemList.put(ItemList.BOOM, 10000);
+		itemList.put(ItemList.HAMMER, 5000);
+		itemList.put(ItemList.NEWMAP, 10000);
+	}
+	
+	/**
+	 * 扣除金币
+	 * @param user
+	 * @param coin
+	 * @return 返回TRUE表示金币足够，购买成功
+	 */
+	private static boolean deductCoin(User user, int coin) {
+		if (user.getCoinCount() >= coin) {
+			user.setCoinCount(user.getCoinCount() - coin);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * 购买商品
+	 * @param user 购买者
+	 * @param item 商品名称
+	 * @return 购买成功返回TRUE，扣除相应金币，否则返回FALSE
+	 */
+	public static boolean buyItem(User user, String item) {
+		return deductCoin(user, itemList.get(item));
+	}
+	
+	public static HashMap<String, Integer> getItemList() {
+		return itemList;
+	}
+	
+}
