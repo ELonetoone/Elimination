73ebdd42f55d7d4627364f40cccf355f8deaaa41
訾源 <161250220@smail.nju.edu.cn>
2017-05-12 07:57:14 +0800

    增加服务器端代码健壮性

--- ELServer/Server.py
+++ ELServer/Server.py
@@ -5,27 +5,31 @@ udpSock = ELSocket(21445)
 db = DataBase()
 
 while True:
-    print 'waiting for connection!'
-    data, address = udpSock.receive()
-    datas = data.split()
-    if datas[0] == 'register':
-        feedInfo = db.regitser(datas[1:])
-    elif datas[0] == 'login':
-        feedInfo = db.login(datas[1:])
-    elif datas[0] == 'update':
-        feedInfo = db.update(datas[1:])
-    elif datas[0] == 'topUp':
-        feedInfo = db.topUp(datas[1:])
-    else:
-        feedInfo = "wrong message"
-    udpSock.sendto(feedInfo, address)
-    print data
     try:
-        log = open('log.txt', 'a')
-        log.write(str(address) + ' ' + data + ' ' +  feedInfo + '\n')
-    except Exception as e:
-        print 'log Error!'
-        print e
-    finally:
-        log.close()
+        print 'waiting for connection!'
+        data, address = udpSock.receive()
+        datas = data.split()
+        if datas[0] == 'register':
+            feedInfo = db.regitser(datas[1:])
+        elif datas[0] == 'login':
+            feedInfo = db.login(datas[1:])
+        elif datas[0] == 'update':
+            feedInfo = db.update(datas[1:])
+        elif datas[0] == 'topUp':
+            feedInfo = db.topUp(datas[1:])
+        else:
+            feedInfo = "wrong message"
+            udpSock.sendto(feedInfo, address)
+            print data
+            try:
+                log = open('log.txt', 'a')
+                log.write(str(address) + ' ' + data + ' ' +  feedInfo + '\n')
+            except Exception as e:
+                print 'log Error!'
+                print e
+            finally:
+                    log.close()
+    except Exception as err:
+        print "some error"
+    
 
