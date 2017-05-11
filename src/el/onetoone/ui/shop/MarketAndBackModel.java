8bb4e53b0763871cc174d0dac9d06fd410563c18
Junda Liao <284983325@qq.com>
2017-05-12 00:12:54 +0800

    改变了界面改变方式，好多都要重写，明天再写

--- src/el/onetoone/ui/shop/MarketAndBackModel.java
+++ src/el/onetoone/ui/shop/MarketAndBackModel.java
@@ -1,30 +1,29 @@
 package el.onetoone.ui.shop;
 
 import el.onetoone.back.Config;
-import el.onetoone.ui.FunctionModel;
 import el.onetoone.ui.SystemButton;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MarketAndBackModel implements FunctionModel{
-	
+public class MarketAndBackModel {
+
 	private Scene marketAndBackScene;
-	
+
 	private MarketPanel marketPanel;
-	
-	//返回按钮
+
+	// 返回按钮
 	private SystemButton backButton;
-	
+
 	/**
 	 * 保留指向综合界面的引用，返回综合界面
 	 */
 	private Scene synScene;
-	
+
 	/**
 	 * 主界面stage
 	 */
 	private Stage stage;
-	
+
 	public MarketAndBackModel(Stage stage, Scene synScene) {
 		this.synScene = synScene;
 		this.stage = stage;
@@ -33,50 +32,29 @@ public class MarketAndBackModel implements FunctionModel{
 	/**
 	 * 初始化
 	 */
-	@Override
 	public void init() {
-		
+
 		marketPanel = new MarketPanel();
 		marketAndBackScene = new Scene(marketPanel, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
 		stage.setScene(marketAndBackScene);
-		
+
 		createNode();
 		addNode();
-		
+
 	}
-	
+
 	private void addNode() {
-		
+
 		marketPanel.getChildren().add(backButton);
 	}
 
 	private void createNode() {
-		
+
 		backButton = new SystemButton(0);
 		backButton.setLayoutX(Config.SCREEN_WIDTH - 55);
 		backButton.setLayoutY(10);
 		backButton.setOnAction(e -> {
-			returnToSyn();
 		});
 	}
 
-	/**
-	 * 获取场景
-	 * @return 对应的场景
-	 */
-	@Override
-	public Scene getScene() {
-		return this.marketAndBackScene;
-	}
-	
-	/**
-	 * 返回综合界面
-	 */
-	@Override
-	public void returnToSyn() {
-		stage.setScene(synScene);
-	}
-	
 }
-
-
