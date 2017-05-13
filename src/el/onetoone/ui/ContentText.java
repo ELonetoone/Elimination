d749210be91d69fae76cdd295c95c2250d34bb3c
訾源 <161250220@smail.nju.edu.cn>
2017-05-13 21:13:40 +0800

    类名改了，sourcetree崩了。。

--- src/el/onetoone/ui/ContentText.java
+++ src/el/onetoone/ui/ContentText.java
@@ -8,26 +8,26 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class ContentText extends Text{
-
-	BaseDiamondGrid diamondGrid;
-	
-	public ContentText(BaseDiamondGrid diamondGrid) {
-		this.diamondGrid = diamondGrid;
-		setFont(Font.font("Comic Sans MS"));
-		setStyle("-fx-fill: #ff00ff;"
-				+ "-fx-text-alignment: center;"
-				+ "-fx-font-size: 24");
-	}
-	
-	public void bindGrade() {
-		textProperty().bind(diamondGrid.gradeProperty().asString());
-	}
-	
-	public void bindStep() {
-		textProperty().bind(diamondGrid.stepProperty().asString());
-	}
-	
-	public void bindTime() {
-		textProperty().bind(diamondGrid.timeProperty().asString());
-	}
+    
+    BaseDiamondGrid diamondGrid;
+    
+    public ContentText(BaseDiamondGrid diamondGrid) {
+        this.diamondGrid = diamondGrid;
+        setFont(Font.font("Comic Sans MS"));
+        setStyle("-fx-fill: #ff00ff;"
+                 + "-fx-text-alignment: center;"
+                 + "-fx-font-size: 24");
+    }
+    
+    public void bindGrade() {
+        textProperty().bind(diamondGrid.gradeProperty().asString());
+    }
+    
+    public void bindStep() {
+        textProperty().bind(diamondGrid.stepProperty().asString());
+    }
+    
+    public void bindTime() {
+        textProperty().bind(diamondGrid.timeProperty().asString());
+    }
 }
